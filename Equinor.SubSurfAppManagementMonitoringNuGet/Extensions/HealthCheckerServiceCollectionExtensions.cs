a1b41c66893cc354a7ca019ebba64933d94cf139
robinaasan <robin.aasan@outlook.com>
2024-12-13 10:14:47 +0100

    fix: environment registered as singleton

--- Equinor.SubSurfAppManagementMonitoringNuGet/Extensions/HealthCheckerServiceCollectionExtensions.cs
+++ Equinor.SubSurfAppManagementMonitoringNuGet/Extensions/HealthCheckerServiceCollectionExtensions.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Equinor.SubSurfAppManagementMonitoringNuget.Extensions;
 
-static class HealthCheckerServiceCollectionExtensions
+public static class HealthCheckerServiceCollectionExtensions
 {
     /// <summary>
     /// Adds the default health checker service and integrates it with the health check builder.
@@ -12,7 +12,7 @@ static class HealthCheckerServiceCollectionExtensions
     /// <param name="services">The service collection.</param>
     public static IHealthChecksBuilder AddDefaultHealthChecker(this IServiceCollection services)
     {
-        services.AddTransient<IEnvironment, EnvironmentWrapper>();
+        services.AddSingleton<IEnvironment, EnvironmentWrapper>();
         services.AddTransient<IHealthCheckerService, DefaultHealthCheckerService>();
         var healthChecksBuilder = services.AddHealthChecks();
 
