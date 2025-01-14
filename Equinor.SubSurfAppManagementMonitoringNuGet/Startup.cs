bdbd70ef2fac3cb17feed508783c3d2804a31517
robinaasan <robin.aasan@outlook.com>
2025-01-14 15:03:09 +0100

    feat: added TelemetryInitializer and Startup where its configured

---
+++ Equinor.SubSurfAppManagementMonitoringNuGet/Startup.cs
@@ -0,0 +1,25 @@
+using Equinor.SubSurfAppManagementMonitoringNuGet.Config;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Equinor.SubSurfAppManagementMonitoringNuGet;
+
+public static class Startup
+{
+    /// <summary>
+    /// Configures the default services and dependencies for the nuget package
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="Configuration"></param>
+    public static void Configure(IServiceCollection services, IConfiguration Configuration)
+    {
+        ConfigureDependencyInjection(services);
+    }
+    
+    private static void ConfigureDependencyInjection(IServiceCollection services)
+    {
+        services.AddSingleton<ITelemetryInitializer, AuditTelemetryInitializer>();
+    }
+
+}
